cba7212a35ac3ad327ae8b5d2f779fd0fac0531d
Miguel Rios R <mgl.rios.r@gmail.com>
2020-03-22 17:01:28 -0500

    Constants added

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBdjQ59A20hrVp9obadrIw2IpeSx1V2Woo"></script>
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+
+const Constants = () => {
+  var google_map_apiKey = "AIzaSyBdjQ59A20hrVp9obadrIw2IpeSx1V2Woo"
+}
+
+export default Constants;
