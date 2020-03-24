8b87770bbf7439bd7b0fc298b331a95c21c0e380
Miguel Rios R <mgl.rios.r@gmail.com>
2020-03-24 16:43:20 -0500

    Data struct added

--- src/components/covidEcMap/CovidEcMap.js
+++ src/components/covidEcMap/CovidEcMap.js
@@ -1,49 +1,65 @@
 import React, { Component } from 'react';
-import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps"
+import { GoogleMap, Marker, withGoogleMap, withScriptjs, InfoWindow, LoadScript, Circle } from "react-google-maps"
 
 import MapTheme from './theme'
 
 const MapBuild = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={7}
-      defaultCenter={{ lat: -1.9382072, lng: -78.3260329 }}
-      defaultOptions={{ styles: MapTheme }}
+  withGoogleMap(props => {
+
+    const onLoad = infoWindow => {
+      console.log('infoWindow: ', infoWindow)
+    }
+    let infoWindowActive = false;
+    let markers = props.data.map((item, key) =>
+    <Marker key = {key} position = { item.location } onLoad={onLoad} onClick = {() => handleToggleOpen()} >
+      <InfoWindow key = {key} position = { item.location } onLoad={onLoad} > 
+        <h1>{ item.city }</h1>
+      </InfoWindow>
+    </Marker>
+  )
+
+    function handleToggleOpen(){
+      infoWindowActive = true
+      alert()
+    }
+
+    return (
+      <GoogleMap
+      defaultZoom = {7}
+      defaultCenter = { { lat: -1.9382072, lng: -78.3260329 } }
+      defaultOptions = {{ styles: MapTheme }}
     >
-      <Marker
-          key={'i'}
-          position={{ lat:  -2.1524996, lng: -80.050141}}
-          onClick={() => props.handleToggleOpen()} 
-      ></Marker>
-      <Marker
-          key={'i'}
-          position={{ lat:  -0.1865938, lng: -78.5706235}}
-          onClick={() => props.handleToggleOpen()}
-      ></Marker>
-      <Marker
-          key={'ii'}
-          position={{ lat:  -1.2571434, lng: -78.6566383}}
-          onClick={() => props.handleToggleOpen()}
-      ></Marker>
-      <Marker
-          key={'iii'}
-          position={{ lat:  -2.8922687, lng: -79.0243995}}
-          onClick={() => props.handleToggleOpen()}
-      ></Marker>
+    { markers }
     </GoogleMap>
-    
-  ))
+    )
+  })
 );
 
 class Map extends Component {
 
-  handleToggleOpen(){
-    alert('Desplegar los datos')
+  constructor(props) {
+    super(props);
+    this.state = {
+      actived: true,
+      ubications: []
+    };
+  }
+
+  componentDidMount(){
+    this.setState({
+      data: [
+        {
+          location: { lat:  -2.1524996, lng: -80.050141 },
+          city: 'Guayaquil'
+        }
+      ]
+    })
   }
 
   render(){
     return <MapBuild
-      handleToggleOpen = {this.handleToggleOpen}
+      activated = {this.state.actived}
+      data = {this.state.data}
       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXRGdzHI4vx-HDA8tukA2SscbZjuuzg-4"
       loadingElement={<div style={{ width: `100%`, height: `100%` }} />}
       containerElement={<div style={{ width: `100%`, height: `100vh` }} />}
